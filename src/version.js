fac502acf9a358225b3664cd1fa9feab99b6d8d2
rambod <rambodc@irobotx.io>
2026-04-23 10:25:13 -0600

    fix github

--- src/version.js
+++ src/version.js
@@ -1,7 +1,7 @@
 // src/version.js
 // Edit this value to show a visible build number on the Home page.
 // Example: 'DEV-001', 'DEV-002', 'PROD-2024-09-10', etc.
-export const UI_BUILD_TAG = 'DEV-2';
+export const UI_BUILD_TAG = 'DEV-21';
 
 
 
